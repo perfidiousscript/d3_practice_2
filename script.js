cd52c2278082b6e1f0a9f8b652f3a37c6c1ea9e6
Sam Moss <sam.moss321@gmail.com>
2015-12-28 10:01:50 -0600

    click handler added

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 /**
  * Created by samuelmoss on 12/28/15.
  */
+
+console.log("script is loading");
+
+
 var svg = d3.select("body").append("svg");
 
 svg.attr("height", 500);
 svg.attr("width", 900);
+
+var dataset = [50,100,150,50,100,250,450];
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("height", function(d){ return d })
+    .attr("width", 50)
+    .style("fill", "red")
+    .attr("x", function(d,i){return i * 60})
+    .on("click", function(d, i) {
+        console.log(d);
+        });
+
+
+//svg.attr("height", 500);
+//svg.attr("width", 900);
+//
+//svg.append("rect")
+//    .attr("height", 50)
+//    .attr("width", 50)
+//    .style("fill", "red");
